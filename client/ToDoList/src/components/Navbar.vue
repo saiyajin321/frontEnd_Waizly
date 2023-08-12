1135a4dcdb5beea0684ae28e44b3bf3ed16947ec
saiyajin321 <iqbalgautawa@gmail.com>
2023-08-12 12:53:25 +0700

    final

--- client/ToDoList/src/components/Navbar.vue
+++ client/ToDoList/src/components/Navbar.vue
@@ -31,10 +31,6 @@ export default{
         </RouterLink>
       </div>
 
-      <div class=" relative ">
-    <input type="text" id="rounded-email" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Search"/>
-    </div>
-
       <div class="flex items-center">
           <div class="flex items-center ml-3">
             <div>
--- client/ToDoList/src/views/Home.vue
+++ client/ToDoList/src/views/Home.vue
@@ -6,6 +6,11 @@ import { mapActions, mapState } from 'pinia';
 import { useToDoStore } from '../stores/toDo';
 
 export default{
+  data(){
+    return{
+      search : ''
+    }
+  },
     components : {
         Sidebar,
         Navbar,
@@ -19,7 +24,11 @@ export default{
     await this.fetchTodo()
   },
   computed : {
-    ...mapState(useToDoStore, ['toDos'])
+    ...mapState(useToDoStore, ['toDos']),
+
+    filteredPosts(){
+      return this.toDos.filter(el => el.todo.toLowerCase().includes(this.search.toLowerCase()))
+    }
   }
 }
 
@@ -30,14 +39,18 @@ export default{
 <Navbar/>
 <Sidebar/>
 
-<div class="p-4 sm:ml-64">
-   <div class="p-4 border-2 border-gray-200 rounded-lg dark:border-gray-700 mt-14">
+<div class="p-4 sm:ml-64 mt-2">
+    <div class=" relative mt-12 mb-2">
+      <input v-model="search" type="text" id="rounded-email" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Search"/>
+    </div>
+   <div class="p-4 border-2 border-gray-200 rounded-lg dark:border-gray-700 ">
       <div class="grid grid-cols-4 gap-4 mb-4">
-        <Card v-for="toDo in toDos" :key="toDo.id" :toDo="toDo"/>
+        <Card v-for="toDo in filteredPosts" :key="toDo.id" :toDo="toDo"/>
 
       </div>
      
    </div>
+
 </div>
 
 </template>
